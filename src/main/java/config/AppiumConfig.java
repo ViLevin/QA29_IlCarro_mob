af8adb9c1ca6540d2b52851ede79a6c4bd9439a2
ViLevin <vilevinqa@gmail.com>
2025-06-19 19:35:15 +0300

    Java_11

--- src/main/java/config/AppiumConfig.java
+++ src/main/java/config/AppiumConfig.java
@@ -1,4 +1,45 @@
 package config;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AppiumConfig {
+    public static AppiumDriver<AndroidElement> driver;
+
+    @BeforeMethod
+    public void setUp() {
+//                "platformName":"Android",
+//                "deviceName":"Vi_nexus",
+//                "platformVersion":"8.1.0",
+//                "appPackage":"com.telran.ilcarro",
+//                "appActivity":".SplashActivity"
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "Vi_nexus");
+        desiredCapabilities.setCapability("platformVersion", "8.1.0");
+        desiredCapabilities.setCapability("appPackage", "com.telran.ilcarro");
+        desiredCapabilities.setCapability("appActivity", ".SplashActivity");
+
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        String url = "http://localhost:4723/wd/hub";
+        try {
+            driver = new AppiumDriver<>(new URL(url), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
 }
--- src/test/java/tests/SplashTests.java
+++ src/test/java/tests/SplashTests.java
@@ -1,4 +1,14 @@
 package tests;
 
-public class SplashTests {
+import config.AppiumConfig;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import screens.SplashScreen;
+
+public class SplashTests extends AppiumConfig {
+
+    @Test
+    public void validateVersion(){
+        Assert.assertTrue(new SplashScreen(driver).validateVersion());
+    }
 }
